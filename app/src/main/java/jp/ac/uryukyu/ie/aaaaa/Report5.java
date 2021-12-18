1314bd5ee8adc59ba0d993bf59f8b55cd9bb7015
Uechi Ryota <e215752@ie.u-ryukyu.ac.jp>
2021-12-18 14:49:47 +0900

    3rd commit

--- app/src/main/java/jp/ac/uryukyu/ie/aaaaa/Report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/aaaaa/Report5.java
@@ -3,9 +3,10 @@ package jp.ac.uryukyu.ie.aaaaa;
 public class Report5 {
     public static void main(String[] args) {
         try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
+
+        } catch (NumberFormatException e) {
             System.out.println("例外状況のエラーメッセージ出力");
             System.out.println(e.getMessage());
         }
